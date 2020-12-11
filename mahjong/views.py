badd183aaa0835619ab487d5d695a42a318559af
EC2 Default User <ec2-user@ip-172-31-42-119.ec2.internal>
2020-12-11 05:51:50 +0000

    create test record

--- mahjong/views.py
+++ mahjong/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.utils import timezone
+from .models import Mahjong
 
 def index (request):
-    return render(request, 'mahjong/index.html', {})
+    currentTime = timezone.now()
+    return render(request, 'mahjong/index.html', {'time' : currentTime})
